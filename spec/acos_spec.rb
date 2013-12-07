b381a172485893b26d179ef9473ed59b3e869b89
Sam Rawlins <sam.rawlins@gmail.com>
2013-12-07 15:39:45 -0800

    bumping rspec and cleaning up specs to use expect()... alphabetically

--- spec/acos_spec.rb
+++ spec/acos_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 # All tests adapted from MPC 1.0.1's tests/acos.dat
 describe MPC, '#acos' do
-  it 'should calculate the inverse cosine of a pure real argument' do
+  it 'calculates the inverse cosine of a pure real argument' do
     data = [
       [[ "0x1921FB54442D18p-51", 53, 16], [ "0x13D2B7539DBA4Cp-51", 53, 16], MPC.new([GMP::F(-6  ), -GMP::F(0)])],
       [[ "0x1921FB54442D18p-51", 53, 16], ["-0x13D2B7539DBA4Cp-51", 53, 16], MPC.new([GMP::F(-6  ),  GMP::F(0)])],
@@ -23,12 +23,12 @@ describe MPC, '#acos' do
     ]
     data.each do |expected_real, expected_imag, input|
       actual = input.acos
-      actual.real.should eq GMP::F.new(*expected_real)
-      actual.imag.should eq GMP::F.new(*expected_imag)
+      expect(actual.real).to eq GMP::F.new(*expected_real)
+      expect(actual.imag).to eq GMP::F.new(*expected_imag)
     end
   end
 
-  it 'should calculate the inverse cosine of a pure imaginary argument' do
+  it 'calculates the inverse cosine of a pure imaginary argument' do
     data = [
       [["0x1921FB54442D18p-52", 53, 16], [ "0x1D185B507EDC0Ep-52", 53, 16], MPC.new([-GMP::F(0), GMP::F(-3   )])],
       [["0x1921FB54442D18p-52", 53, 16], [ "0x1D185B507EDC0Ep-52", 53, 16], MPC.new([ GMP::F(0), GMP::F(-3   )])],
@@ -41,26 +41,26 @@ describe MPC, '#acos' do
     ]
     data.each do |expected_real, expected_imag, input|
       actual = input.acos
-      actual.real.should eq GMP::F.new(*expected_real)
-      actual.imag.should eq GMP::F.new(*expected_imag)
+      expect(actual.real).to eq GMP::F.new(*expected_real)
+      expect(actual.imag).to eq GMP::F.new(*expected_imag)
     end
   end
 
-  it 'should calculate the inverse cosine with other precisions' do
+  it 'calculates the inverse cosine with other precisions' do
     actual = MPC.new([GMP::F.new(2), GMP::F.new(1)]).acos(MPC::MPC_RNDNZ, 2)
-    actual.real.should eq GMP::F.new( 0.5, 2)
-    actual.imag.should eq GMP::F.new(-1,   2)
+    expect(actual.real).to eq GMP::F.new( 0.5, 2)
+    expect(actual.imag).to eq GMP::F.new(-1,   2)
   end
 
-  it 'should calculate the inverse cosine with other precisions' do
+  it 'calculates the inverse cosine with other precisions' do
     actual = MPC.new([GMP::F.new(8.5), GMP::F.new(-71)]).acos(MPC::MPC_RNDNU, 9)
-    actual.real.should eq GMP::F.new("0x5Dp-6", 9, 16)
-    actual.imag.should eq GMP::F.new("0x9Fp-5", 9, 16)
+    expect(actual.real).to eq GMP::F.new("0x5Dp-6", 9, 16)
+    expect(actual.imag).to eq GMP::F.new("0x9Fp-5", 9, 16)
   end
 
-  it 'should calculate the inverse cosine with other precisions' do
+  it 'calculates the inverse cosine with other precisions' do
     actual = MPC.new([GMP::F.new("0x3243F6A8885A3p-48", 53, 16), GMP::F.new("0x162E42FEFA39EFp-53", 53, 16)]).acos
-    actual.real.should eq GMP::F.new("0x74C141310E695p-53", 53, 16)
-    actual.imag.should eq GMP::F.new("-0x1D6D2CFA9F3F11p-52", 53, 16)
+    expect(actual.real).to eq GMP::F.new("0x74C141310E695p-53", 53, 16)
+    expect(actual.imag).to eq GMP::F.new("-0x1D6D2CFA9F3F11p-52", 53, 16)
   end
 end
--- spec/mpc_single_function_args_spec.rb
+++ spec/mpc_single_function_args_spec.rb
@@ -11,31 +11,31 @@ describe "MPC_SINGLE_FUNCTION args" do
   end
   it "should accept no arguments" do
     @functions.each do |func|
-      expect { @z.send(func) }.to_not raise_error(ArgumentError)
+      expect { @z.send(func) }.to_not raise_error
     end
   end
 
   it "should accept a single rounding mode argument, MPC_RNDNN" do
     @functions.each do |func|
-      expect { @z.send(func, MPC::MPC_RNDNN) }.to_not raise_error(ArgumentError)
+      expect { @z.send(func, MPC::MPC_RNDNN) }.to_not raise_error
     end
   end
 
   it "should accept a single rounding mode argument, MPC_RNDZD" do
     @functions.each do |func|
-      expect { @z.send(func, MPC::MPC_RNDZD) }.to_not raise_error(ArgumentError)
+      expect { @z.send(func, MPC::MPC_RNDZD) }.to_not raise_error
     end
   end
 
   it "should accept a single rounding mode argument and a single precision" do
     @functions.each do |func|
-      expect { @z.send(func, MPC::MPC_RNDZZ, 128) }.to_not raise_error(ArgumentError)
+      expect { @z.send(func, MPC::MPC_RNDZZ, 128) }.to_not raise_error
     end
   end
 
   it "should accept a single rounding mode argument and two precisions" do
     @functions.each do |func|
-      expect { @z.send(func, MPC::MPC_RNDZZ, 128, 256) }.to_not raise_error(ArgumentError)
+      expect { @z.send(func, MPC::MPC_RNDZZ, 128, 256) }.to_not raise_error
     end
   end
 end
