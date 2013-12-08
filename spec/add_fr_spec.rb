5267838bb3061a28f0709d0a447be666ec0ac87f
Sam Rawlins <sam.rawlins@gmail.com>
2013-12-07 19:28:34 -0800

    converted another spec

--- spec/add_fr_spec.rb
+++ spec/add_fr_spec.rb
@@ -2,53 +2,53 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 # All tests adapted from MPC 1.0.1's tests/add.dat
 describe MPC, '#add' do
-  it 'should calculate the sum of two real MPCs' do
+  it 'calculates the sum of two real MPCs' do
     op1 = MPC.new([1, 0])
     op2_106 = GMP::F("0x10000000000001p-106", 53, 16)
     actual = op1.add(op2_106, MPC::MPC_RNDNN)
-    actual.real.should eq GMP::F("0x10000000000000p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000000p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
 
     op2_105 = GMP::F("0x10000000000001p-105", 53, 16)
     actual = op1.add(op2_105, MPC::MPC_RNDNN)
-    actual.real.should eq GMP::F("0x10000000000001p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000001p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
 
     op2_104 = GMP::F("0x10000000000001p-104", 53, 16)
     actual = op1.add(op2_104, MPC::MPC_RNDNN)
-    actual.real.should eq GMP::F("0x10000000000001p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000001p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
 
     actual = op1.add(op2_105, MPC::MPC_RNDZZ)
-    actual.real.should eq GMP::F("0x10000000000000p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000000p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
 
     actual = op1.add(op2_105, MPC::MPC_RNDUU)
-    actual.real.should eq GMP::F("0x10000000000001p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000001p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
 
     actual = op1.add(op2_105, MPC::MPC_RNDDD)
-    actual.real.should eq GMP::F("0x10000000000000p-52", 53, 16)
-    actual.imag.should eq GMP::F(0)
+    expect(actual.real).to eq GMP::F("0x10000000000000p-52", 53, 16)
+    expect(actual.imag).to eq GMP::F(0)
   end
 
-  it 'should calculate the sum of an imaginary argument with a complex one.' do
+  it 'calculates the sum of an imaginary argument with a complex one.' do
     op1 = MPC.new([0, 1])
     op2 = GMP::F(1)
     actual = op1.add(op2, MPC::MPC_RNDNN)
-    actual.real.should eq GMP::F(1)
-    actual.imag.should eq GMP::F(1)
+    expect(actual.real).to eq GMP::F(1)
+    expect(actual.imag).to eq GMP::F(1)
 
     actual = op1.add(op2, MPC::MPC_RNDZZ)
-    actual.real.should eq GMP::F(1)
-    actual.imag.should eq GMP::F(1)
+    expect(actual.real).to eq GMP::F(1)
+    expect(actual.imag).to eq GMP::F(1)
 
     actual = op1.add(op2, MPC::MPC_RNDUU)
-    actual.real.should eq GMP::F(1)
-    actual.imag.should eq GMP::F(1)
+    expect(actual.real).to eq GMP::F(1)
+    expect(actual.imag).to eq GMP::F(1)
 
     actual = op1.add(op2, MPC::MPC_RNDDD)
-    actual.real.should eq GMP::F(1)
-    actual.imag.should eq GMP::F(1)
+    expect(actual.real).to eq GMP::F(1)
+    expect(actual.imag).to eq GMP::F(1)
   end
 end
