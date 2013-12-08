8b7f640db1758a27e0e14203de8e43689c7caa76
Sam Rawlins <sam.rawlins@gmail.com>
2013-12-07 23:06:04 -0800

    fixing specs to account for MPC 0.9.4

--- spec/log10_spec.rb
+++ spec/log10_spec.rb
@@ -1,26 +1,34 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 # All tests adapted from MPC 1.0.1's tests/log10.dat
-describe MPC, '#log' do
-  it 'should calculate the base 10 logarithm of +1 +- i*0' do
+describe MPC, '#log10' do
+  it 'calculates the base 10 logarithm of +1 +- i*0' do
+    pending("log10 introduced in MPC 1.0.0") if MPC::MPC_VERSION[0] == "0"
+
     actual = MPC.new([1, 0], 2).log10
-    actual.real.should eq 0
-    actual.imag.should eq 0
+    expect(actual.real).to eq 0
+    expect(actual.imag).to eq 0
   end
 
-  it 'should calculate the base 10 logarithm of 10 +- i*0' do
+  it 'calculates the base 10 logarithm of 10 +- i*0' do
+    pending("log10 introduced in MPC 1.0.0") if MPC::MPC_VERSION[0] == "0"
+
     actual = MPC.new([GMP::F(10, 4), GMP::F(0, 2)]).log10
-    actual.real.should eq 1
-    actual.imag.should eq 0
+    expect(actual.real).to eq 1
+    expect(actual.imag).to eq 0
   end
 
-  it 'should calculate the base 10 logarithm of 100 +- i*0' do
+  it 'calculates the base 10 logarithm of 100 +- i*0' do
+    pending("log10 introduced in MPC 1.0.0") if MPC::MPC_VERSION[0] == "0"
+
     actual = MPC.new([GMP::F(100, 5), GMP::F(0, 2)]).log10
-    actual.real.should eq 2
-    actual.imag.should eq 0
+    expect(actual.real).to eq 2
+    expect(actual.imag).to eq 0
   end
 
-  it 'should calculate the base 10 logarithm of x +i*y with either x or y zero and the other non-zero' do
+  it 'calculates the base 10 logarithm of x +i*y with either x or y zero and the other non-zero' do
+    pending("log10 introduced in MPC 1.0.0") if MPC::MPC_VERSION[0] == "0"
+
     data = [
       [["0x13afeb354b7d97p-52", 53, 16], [ 0,  2],                          MPC.new([GMP::F( "0x11", 5, 16),  GMP::F(0, 2)]), MPC::MPC_RNDNN],
       [["0x13afeb354b7d97p-52", 53, 16], [ "0x15d47c4cb2fba1p-53", 53, 16], MPC.new([GMP::F(0, 2),  GMP::F( "0x11", 5, 16)]), MPC::MPC_RNDNN],
@@ -30,8 +38,8 @@ describe MPC, '#log' do
     ]
     data.each do |expected_real, expected_imag, input, rounding_mode|
       actual = input.log10(rounding_mode)
-      actual.real.should eq GMP::F.new(*expected_real)
-      actual.imag.should eq GMP::F.new(*expected_imag)
+      expect(actual.real).to eq GMP::F.new(*expected_real)
+      expect(actual.imag).to eq GMP::F.new(*expected_imag)
     end
   end
 
